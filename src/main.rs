b14226030d82e4ec3192f8a1b64a1c702dd8ce1d
Taka <71265675+takakv@users.noreply.github.com>
2026-05-06 22:56:55 +0300

    Improve error handling

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,7 @@ use p384::{
     EncodedPoint, NistP384, ProjectivePoint, Scalar, U384,
 };
 use rasn::types::{GeneralString, IntegerType, OctetString};
-use rayon::iter::{IntoParallelIterator, ParallelIterator};
+use rayon::iter::{IndexedParallelIterator, IntoParallelIterator, ParallelIterator};
 use serde::Deserialize;
 use sha2::{digest::Update, Digest, Sha256};
 
@@ -30,6 +30,24 @@ use asn1::structs::{
 mod asn1;
 
 const NUM_BYTES: usize = (Scalar::NUM_BITS / 8) as usize;
+
+#[derive(Debug, thiserror::Error)]
+enum ProofError {
+    #[error("malformed base64: {0}")]
+    MalformedBase64(#[from] base64::DecodeError),
+
+    #[error("malformed ASN.1: {0}")]
+    MalformedAsn1(#[from] rasn::error::DecodeError),
+
+    #[error("invalid point encoding")]
+    InvalidPointEncoding,
+
+    #[error("point is not on the curve")]
+    PointNotOnCurve,
+
+    #[error("scalar out of range")]
+    ScalarOutOfRange,
+}
 
 #[derive(Parser)]
 struct Args {
@@ -59,37 +77,40 @@ fn bytes_to_int(b: &[u8]) -> U384 {
     U384::from_be_slice(b)
 }
 
-fn asn1int_to_scalar(i: rasn::types::Integer) -> Scalar {
+fn asn1int_to_scalar(i: rasn::types::Integer) -> Result<Scalar, ProofError> {
     // We know that the bytes are unsigned.
     // However, if the first bit is set, i.to_unsigned_bytes_be() will prepend a 0-byte.
-    // This will cause Scalar::from_slice() to panic due to an incompatible length.
+    // This will cause Scalar::from_slice() to fail due to an incompatible length.
     // There is also a small probability that there are 8 or more leading 0-bits.
     let (bytes, len) = i.to_unsigned_bytes_be();
     let slice = bytes.as_ref();
 
-    match len.cmp(&NUM_BYTES) {
-        std::cmp::Ordering::Equal => Scalar::from_slice(slice).unwrap(),
-        std::cmp::Ordering::Greater => Scalar::from_slice(&slice[1..]).unwrap(),
+    Ok(match len.cmp(&NUM_BYTES) {
+        std::cmp::Ordering::Equal => Scalar::from_slice(slice),
+        std::cmp::Ordering::Greater => Scalar::from_slice(&slice[1..]),
         std::cmp::Ordering::Less => {
             let mut buf = [0u8; NUM_BYTES];
             buf[NUM_BYTES - len..].copy_from_slice(&slice[..len]);
-            Scalar::from_slice(&buf).unwrap()
+            Scalar::from_slice(&buf)
         }
     }
+    .map_err(|_| ProofError::ScalarOutOfRange)?)
 }
 
-fn der_to_point(octets: &[u8]) -> ProjectivePoint {
-    let point: OctetString = rasn::der::decode(octets).unwrap();
+fn der_to_point(octets: &[u8]) -> Result<ProjectivePoint, ProofError> {
+    let point: OctetString = rasn::der::decode(octets)?;
     octets_to_point(&point)
 }
 
-fn octets_to_point(octets: &[u8]) -> ProjectivePoint {
-    let encoded = EncodedPoint::from_bytes(octets).unwrap();
-    ProjectivePoint::from_encoded_point(&encoded).unwrap()
+fn octets_to_point(octets: &[u8]) -> Result<ProjectivePoint, ProofError> {
+    let encoded = EncodedPoint::from_bytes(octets).map_err(|_| ProofError::InvalidPointEncoding)?;
+    ProjectivePoint::from_encoded_point(&encoded)
+        .into_option()
+        .ok_or(ProofError::PointNotOnCurve)
 }
 
-fn b64decode(s: &str) -> Vec<u8> {
-    general_purpose::STANDARD.decode(s).unwrap()
+fn b64decode(s: &str) -> Result<Vec<u8>, ProofError> {
+    Ok(general_purpose::STANDARD.decode(s)?)
 }
 
 static NI_PROOF_DOMAIN: OnceLock<GeneralString> = OnceLock::new();
@@ -145,55 +166,54 @@ fn compute_challenge(seed: &[u8], upper_bound: U384) -> Scalar {
     Scalar::from_slice(&num.to_be_bytes()).unwrap()
 }
 
-fn verify_proof(package: ProofPackage, pubkey: &PublicKey) -> bool {
-    let ciphertext_bin = b64decode(&package.ciphertext);
-    let message_bin = b64decode(&package.message);
-    let proof_bin = b64decode(&package.proof);
+fn verify_proof(package: ProofPackage, pubkey: &PublicKey) -> Result<bool, ProofError> {
+    let ciphertext_bin = b64decode(&package.ciphertext)?;
+    let message_bin = b64decode(&package.message)?;
+    let proof_bin = b64decode(&package.proof)?;
 
-    let ciphertext_asn1: EncryptedBallot = rasn::der::decode(&ciphertext_bin).unwrap();
-    let proof_asn1: DecryptionProof = rasn::der::decode(&proof_bin).unwrap();
+    let ciphertext_asn1: EncryptedBallot = rasn::der::decode(&ciphertext_bin)?;
+    let proof_asn1: DecryptionProof = rasn::der::decode(&proof_bin)?;
 
-    let decrypted = der_to_point(&message_bin);
+    let decrypted = der_to_point(&message_bin)?;
     let decrypted_bin = decrypted.to_encoded_point(false);
 
     let seed = derive_seed(
         &pubkey.spki,
         &ciphertext_asn1,
-        &decrypted_bin.as_bytes(),
+        decrypted_bin.as_bytes(),
         &proof_asn1,
     );
 
     let k = compute_challenge(&seed, NistP384::ORDER);
 
-    let u = octets_to_point(&ciphertext_asn1.cipher.u);
-    let v = octets_to_point(&ciphertext_asn1.cipher.v);
-    let a = octets_to_point(&proof_asn1.msg_commitment);
-    let b = octets_to_point(&proof_asn1.key_commitment);
-    let s = asn1int_to_scalar(proof_asn1.response);
+    let u = octets_to_point(&ciphertext_asn1.cipher.u)?;
+    let v = octets_to_point(&ciphertext_asn1.cipher.v)?;
+    let a = octets_to_point(&proof_asn1.msg_commitment)?;
+    let b = octets_to_point(&proof_asn1.key_commitment)?;
+    let s = asn1int_to_scalar(proof_asn1.response)?;
 
     let lhs1 = u * s;
     let rhs1 = a + (v - decrypted) * k;
 
     if !lhs1.eq(&rhs1) {
-        return false;
+        return Ok(false);
     }
 
     let lhs2 = ProjectivePoint::GENERATOR * s;
     let rhs2 = b + pubkey.point * k;
 
-    lhs2.eq(&rhs2)
+    Ok(lhs2.eq(&rhs2))
 }
 
 fn parse_pubkey(pubkey_bin: &[u8]) -> PublicKey {
-    let spki: SubjectPublicKeyInfo = rasn::der::decode(pubkey_bin).unwrap();
+    let spki: SubjectPublicKeyInfo =
+        rasn::der::decode(pubkey_bin).expect("Unable to decode public key SPKI");
     let encapsulated_pk_bin = spki.subject_public_key.as_raw_slice();
-    // let params_bin = spki.algorithm.parameters.clone().unwrap().into_bytes();
-
-    // let params: IVXVPublicKeyParams = rasn::der::decode(&params_bin).unwrap();
-    let pkref: ECPublicKey = rasn::der::decode(encapsulated_pk_bin).unwrap();
+    let pkref: ECPublicKey =
+        rasn::der::decode(encapsulated_pk_bin).expect("Unable to decode EC public key");
 
     PublicKey {
-        point: octets_to_point(&pkref.ec_point),
+        point: octets_to_point(&pkref.ec_point).expect("Unable to decode public key point"),
         spki,
     }
 }
@@ -202,7 +222,7 @@ fn main() {
     let args = Args::parse();
 
     let pubkey_pem_bin = fs::read(&args.public_key).expect("Unable to read key from file");
-    let pubkey_pem = pem::parse(pubkey_pem_bin).unwrap();
+    let pubkey_pem = pem::parse(pubkey_pem_bin).expect("Unable to parse PEM file");
     let pubkey = parse_pubkey(pubkey_pem.contents());
 
     let proofs_json_str =
@@ -228,14 +248,25 @@ fn main() {
     );
 
     let start = Instant::now();
-    proofs_json.proofs.into_par_iter().for_each(|package| {
-        if verify_proof(package, &pubkey) {
-            success_count.fetch_add(1, Ordering::Relaxed);
-        } else {
-            failure_count.fetch_add(1, Ordering::Relaxed);
-        }
-        pb.inc(1);
-    });
+    proofs_json
+        .proofs
+        .into_par_iter()
+        .enumerate()
+        .for_each(|(i, package)| {
+            match verify_proof(package, &pubkey) {
+                Ok(true) => {
+                    success_count.fetch_add(1, Ordering::Relaxed);
+                }
+                Ok(false) => {
+                    failure_count.fetch_add(1, Ordering::Relaxed);
+                }
+                Err(e) => {
+                    failure_count.fetch_add(1, Ordering::Relaxed);
+                    pb.println(format!("Proof {i}: {e}"));
+                }
+            }
+            pb.inc(1);
+        });
     pb.finish();
 
     let duration = start.elapsed();
